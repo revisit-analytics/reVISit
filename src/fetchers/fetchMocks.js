fcb3d24e6b4270a3f2b19560a07c633f2d49795a
revisit-analytics <zacharycutler@Zacharys-MacBook-Pro.local>
2020-09-16 14:58:49 -0600

    making changes so it runs

--- src/fetchers/fetchMocks.js
+++ src/fetchers/fetchMocks.js
@@ -1,5 +1,5 @@
 import mockAllData from "./mockData.json";
-let local = true;
+let local = false;
 let host = local ? "http://127.0.0.1:5000" : "http://3.20.123.182:5000";
 
 export async function fetchProvenanceDataByNodeId(nodeId) {
